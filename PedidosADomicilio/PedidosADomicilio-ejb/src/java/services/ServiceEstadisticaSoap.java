23916ecc998c11689157a8a8d492f49c5c4b85e1
David <David@Normandy>
2015-11-26 16:29:17 +0100

    Añadido servicio SOAP

--- PedidosADomicilio/PedidosADomicilio-ejb/src/java/services/ServiceEstadisticaSoap.java
+++ PedidosADomicilio/PedidosADomicilio-ejb/src/java/services/ServiceEstadisticaSoap.java
@@ -5,6 +5,11 @@
  */
 package services;
 
+import ea.ejb.PedidoFacade;
+import ea.ejb.UsuarioRegistradoFacade;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
 
@@ -15,10 +20,34 @@ import javax.ejb.LocalBean;
 @Singleton
 @LocalBean
 public class ServiceEstadisticaSoap {
-
+    @EJB
+    private UsuarioRegistradoFacade usuarioRegistradoFacade;
+    @EJB
+    private PedidoFacade pedidoFacade;
+    
+    //Devuelve el numero de usuarios del sistema.
     public int getNumUsuarios(){
-        int num=0;
-        
+        int num=-1;
+        num = usuarioRegistradoFacade.count();
         return num;
+    }
+    
+    
+    public String[] getTopFiveMenus(){
+        
+        List lista = pedidoFacade.menusMasPedidos();
+        
+        String[] definitiva;
+        definitiva = new String[10];
+        for(int i=0;i<5;i++){
+            if(i+1>lista.size()){
+                break;
+            }
+            Object[] temp =  (Object[]) lista.get(i);
+            int e = i*2;
+            definitiva[e]= (String)temp[0];
+            definitiva[e+1]= Integer.toString((Integer)temp[1]);
+        }
+        return definitiva;
     }
 }
